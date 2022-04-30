c43760ff09301da48118fb29e4adfdea7b8aaaf8
Angel Herrarte <angelherrarte3@gmail.com>
2022-04-30 14:25:35 -0600

    Se añadió función para editar serie

--- database_access.py
+++ database_access.py
@@ -1,6 +1,9 @@
 import csv;
 import data_validation;
 
+#Nombres de las columnas
+fieldnames = ['serie', 'estado', 'duracion_capitulo', 'capitulos_vistos', 'plataforma', 'tiempo_invertido'];
+
 #Variables que contienen las plataformas y los estados de una película
 movie_platforms = ['Netflix', 'HBO', 'Disney Plus', 'Primevideo', 'Crunchyoll'];
 movie_states = ['Quiero verla', 'En proceso', 'Dejé de verla', 'Finalizada'];
@@ -17,7 +20,7 @@ def get_movies():
     return movies;
 
 #Devuelve True si la película se añadió
-#Devuelve False si la película no se añadió
+#Devuelve False si la película no se añadió (dato incorrecto)
 def add_movie(serie, estado, duracion_capitulo, capitulos_vistos, plataforma):
     try:
         duracion_capitulo = int(duracion_capitulo);
@@ -42,7 +45,6 @@ def add_movie(serie, estado, duracion_capitulo, capitulos_vistos, plataforma):
             return False;
         
         file = open('database.csv', 'a', newline='', encoding='utf-8');
-        fieldnames = ['serie', 'estado', 'duracion_capitulo', 'capitulos_vistos', 'plataforma', 'tiempo_invertido'];
         file_writer = csv.DictWriter(file, fieldnames=fieldnames);
         file_writer.writerow({
             'serie': serie.title(),
@@ -58,3 +60,42 @@ def add_movie(serie, estado, duracion_capitulo, capitulos_vistos, plataforma):
         
     except:
         return False;
+
+#Actualizar dato de película según indíce, field puede ser 'estado' o 'capitulos_vistos'
+#Devuelve True si la película se edito o False si un dato es incorrecto
+def update_movie(index, field, value):
+    try:
+        is_valid = False;
+        
+        if (field == 'estado'):
+            is_valid = data_validation.validate_string(value);
+            
+        if (field == 'capitulos_vistos'):
+            value = int(value);
+            is_valid = data_validation.validate_int(value);
+        
+        if not is_valid: return False;
+        
+        movies = get_movies();
+        if len(movies) == 0: return True;
+        
+        movie = movies[index];
+        movie[field] = value;
+        movies[index] = movie;
+        
+        file = open('database.csv', 'w', newline='', encoding='utf-8');
+        file_writer = csv.DictWriter(file, fieldnames=fieldnames);
+        
+        file_writer.writeheader();
+        file_writer.writerows(movies);
+        file.close();
+        
+        return True;
+        
+    except:
+        return False;
+
+#Ejemplos
+#print(get_movies());
+#add_movie('Mindhunter', movie_states[0], 20, 0, movie_platforms[0]);
+#update_movie(1, fieldnames[1], movie_states[2]);
